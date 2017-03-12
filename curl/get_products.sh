b873f5504d12386fe243a3256b4731260b9bb8db
Nicolas Dupont <nicolas@akeneo.com>
2017-03-12 18:33:39 +0100

    Add basic bash / curl snippet

---
+++ curl/get_products.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type:application/json" -H "Authorization:Bearer OTdjMGVkNWJkNGIwODFiZjYyMDBiMzdlNjdhOGYxYWRmOTg4NDkwMzI2NDc1MGZhMDIxOTUyMDBhYTQ2Yzg0MQ" http://pim-ce-17-odm.local/app_dev.php/api/rest/v1/products
---
+++ curl/get_token.sh
@@ -0,0 +1,2 @@
+curl -X POST -H "Content-Type:application/json" -H "Authorization:Basic Ml8xeGtrdnhyamt1NzRrNHNzOGs0NGt3d3NrbzQwbzA4Y2tnNDg4b2djb3Nnb29rZ280MDozcHM0aTZxNjJyNnMwdzg4a3Nvc3NvMGNjOHM0d2NjMHc0dzRnY29nbzRnd2dra3drNA==" -d '{"grant_type":"password","username":"admin","password":"admin"}' http://pim-ce-17-odm.local/app_dev.php/api/oauth/v1/token
+
