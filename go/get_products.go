5cf104464dc5564d6d933c0b095e3001f63e48be
Nicolas Dupont <nicolas@akeneo.com>
2017-03-13 13:30:07 +0100

    Adds search argument in get products (Go)

--- go/get_products.go
+++ go/get_products.go
@@ -11,7 +11,8 @@ import (
 
 // build: go build get_products.go
 // make executable: chmod +x ./get_products
-// execute: ./get_products --host http://pim-ce-17-odm.local/app_dev.php --accesstoken YzE2Mjc1NzkxMWUwOGFmMjRiNDJmYzcyYTZlODY1OWNkOWEwMjE4ODY5ZTU1YmNkMzEzZjE3ZDhlZDczNDRhNA
+// execute without filters: ./get_products --host http://pim-ce-17-odm.local/app_dev.php --accesstoken YzE2Mjc1NzkxMWUwOGFmMjRiNDJmYzcyYTZlODY1OWNkOWEwMjE4ODY5ZTU1YmNkMzEzZjE3ZDhlZDczNDRhNA
+// execute with filters: ./get_products --host http://pim-ce-17-odm.local/app_dev.php --accesstoken YzE2Mjc1NzkxMWUwOGFmMjRiNDJmYzcyYTZlODY1OWNkOWEwMjE4ODY5ZTU1YmNkMzEzZjE3ZDhlZDczNDRhNA --search '{"enabled":[{"operator":"=","value":true}]'
 
 func main() {
 
@@ -19,11 +20,16 @@ func main() {
         log.Fatal("Must be used with --host and --accesstoken arguments")
     }
 
-    host := flag.String("host", "http://pim-ce-17-odm.local/app_dev.php", "Akeneo PIM host")
-    accessToken := flag.String("accesstoken", "accesstoken", "Akeneo PIM web api access token")
+    host := flag.String("host", "your host", "Akeneo PIM host")
+    accessToken := flag.String("accesstoken", "your access token", "Akeneo PIM web api access token")
+    search := flag.String("search", " ", "Akeneo PIM web api search filters")
     flag.Parse()
 
     apiUrl := *host + "/api/rest/v1/products"
+    if *search != " " {
+        apiUrl += "?search=" + *search;
+    }
+
     tokenType := "Bearer";
 
     client := http.Client{}
